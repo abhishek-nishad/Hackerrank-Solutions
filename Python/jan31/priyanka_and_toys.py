57ab457b8fc244204bff037f9929883f678acc39
abhishek-nishad <abhishek.nishad.1069@gmail.com>
2022-02-16 15:59:00 +0530

    jan31

---
+++ Python/jan31/priyanka_and_toys.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'toys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY w as parameter.
+#
+def toys(w):
+    w.sort()
+    temp = w[0]
+    count = 1
+    for i in w:
+        if i > temp + 4:
+            temp = i
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    w = list(map(int, input().rstrip().split()))
+
+    result = toys(w)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
